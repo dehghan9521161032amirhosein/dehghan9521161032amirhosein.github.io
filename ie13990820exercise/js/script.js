16bed1967687f842331d366af8e74e55d8bc4f09
dehghan9521161032amirhosein <dehghan.amirhossein.9521161032@gmail.com>
2020-12-06 10:24:06 +0330

    slide 3

---
+++ ie13990820exercise/js/script.js
@@ -0,0 +1,7 @@
+console.log(document.getElementById('main_parag'));
+let parag = document.getElementById('main_parag');
+console.log(parag);
+parag.textContent = 'change by amir';
+parag.innerText = 'goodbye';
+parag.innerHTML = '<h3>Hellllllo</h3>';
+parag.style.borderBottom = 'solid 3px #000';
